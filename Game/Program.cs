4a2fd14994ad214925a2f2c4f124b9fffe9015a6
p4likas51 <schmitzhofer.pal@students.jedlik.eu>
2023-11-23 20:57:20 +0100

    Alap menü rendszer

---
+++ Game/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Game
+{
+    internal partial class Program
+    {
+        static int eletkedv = 70;
+        static int veralkoholszint = 0;
+        static int idegallapot = 0;
+        static int penz = 2000;
+        static void Main(string[] args)
+        {
+            if (foMenu() == 0)
+            {
+                // Ide kerül az első helyszín függvénye, Jelen esetben: Bálint háza pl: BalintHaz()
+            }
+        }
+        public static int Menu(string szoveg, string[] valasztasok)
+        {
+            int valasztottIndex = 0;
+            opciokMegjelenitese();
+            return programFut();
+            void opciokMegjelenitese()
+            {
+                Console.WriteLine(szoveg);
+                for (int i = 0; i < valasztasok.Length; i++)
+                {
+                    string jelenlegiPozicio = valasztasok[i];
+                    if (i == valasztottIndex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine($"<< {jelenlegiPozicio} >>");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                if (valasztasok.Length > 1)
+                {
+                    Console.Write($"\nÉletkedv: {eletkedv}\nIdegállapot: {idegallapot}\nVéralkoholszint: {veralkoholszint}\nPénz: {penz}");
+                }
+
+            }
+            int programFut()
+            {
+                ConsoleKey lenyomottBetu;
+                do
+                {
+                    Console.Clear();
+                    opciokMegjelenitese();
+
+                    ConsoleKeyInfo betuInfo = Console.ReadKey(true);
+                    lenyomottBetu = betuInfo.Key;
+                    if (lenyomottBetu == ConsoleKey.UpArrow)
+                    {
+                        valasztottIndex--;
+                        if (valasztottIndex == -1)
+                        {
+                            valasztottIndex = valasztasok.Length - 1;
+                        }
+                    }
+                    else if (lenyomottBetu == ConsoleKey.DownArrow)
+                    {
+                        valasztottIndex++;
+                        if (valasztottIndex == valasztasok.Length)
+                        {
+                            valasztottIndex = 0;
+                        }
+                    }
+                } while (lenyomottBetu != ConsoleKey.Enter);
+                return valasztottIndex;
+            }
+
+        }
+        public static int foMenu()
+        {
+            string szoveg = @"
+     /$$$$$$$            /$$ /$$             /$$                                                                                                             /$$
+    | $$__  $$          | $$|__/            | $$                                                                                                            | $$
+    | $$  \ $$  /$$$$$$ | $$ /$$ /$$$$$$$  /$$$$$$                /$$$$$$   /$$$$$$  /$$   /$$             /$$$$$$$   /$$$$$$   /$$$$$$        /$$  /$$$$$$ | $$
+    | $$$$$$$  |____  $$| $$| $$| $$__  $$|_  $$_/               /$$__  $$ /$$__  $$| $$  | $$            | $$__  $$ |____  $$ /$$__  $$      |__/ |____  $$| $$
+    | $$__  $$  /$$$$$$$| $$| $$| $$  \ $$  | $$                | $$$$$$$$| $$  \ $$| $$  | $$            | $$  \ $$  /$$$$$$$| $$  \ $$       /$$  /$$$$$$$|__/
+    | $$  \ $$ /$$__  $$| $$| $$| $$  | $$  | $$ /$$            | $$_____/| $$  | $$| $$  | $$            | $$  | $$ /$$__  $$| $$  | $$      | $$ /$$__  $$    
+    | $$$$$$$/|  $$$$$$$| $$| $$| $$  | $$  |  $$$$/            |  $$$$$$$|  $$$$$$$|  $$$$$$$            | $$  | $$|  $$$$$$$| $$$$$$$/      | $$|  $$$$$$$ /$$
+    |_______/  \_______/|__/|__/|__/  |__/   \___/               \_______/ \____  $$ \____  $$            |__/  |__/ \_______/| $$____/       | $$ \_______/|__/
+                                                                             /$$  \ $$ /$$  | $$                                | $$       /$$  | $$              
+                                                                          |  $$$$$$/|  $$$$$$/                                | $$      |  $$$$$$/              
+                                                                           \______/  \______/                                 |__/       \______/               
+            ";
+            string[] valasztasok = { "Játékhoz Nyomj Entert!" };
+            int valasztottIndex = Menu(szoveg, valasztasok);
+            return valasztottIndex;
+        }
+
+    }
+}
